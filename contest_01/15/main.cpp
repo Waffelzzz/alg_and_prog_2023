53b582f0d84a7e73414020361389eb1bc82bd43e
Waffelzzz <109044711+Waffelzzz@users.noreply.github.com>
2023-10-01 14:40:21 +0300

    Update main.cpp

--- contest_01/15/main.cpp
+++ contest_01/15/main.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main(){
+	
+	
+	string str;
+	cin >> str;
+	str = str + "\n";
+	
+	int x = str.size();
+	char k;
+	int count = 0;
+	
+	for(int i = 0;i<(x-1);i++){
+		
+		if(str[i] == str[i+1]){
+			k = str[i];
+			count++; 
+		} else {
+			k = str[i];
+			if(count > 0){
+				cout << k << ++count;
+				count = 0;
+			} else {
+				cout << k;
+				count = 0;
+			}
+		}
+	}
+	return 0;
+}
