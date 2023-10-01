6372b24a96e8ccba1b94e8c039d911064d1ff603
Waffelzzz <109044711+Waffelzzz@users.noreply.github.com>
2023-10-01 14:44:24 +0300

    Update main.cpp

--- contest_01/19/main.cpp
+++ contest_01/19/main.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <set>
+#include <string>
+#include <map>
+using namespace std;
 
+int main() {
+	
+	set <string> slova;
+	set <string>::iterator it;
+	set <string>::iterator it2;
+	map <string, int> countMap;
+	string a;
+	int count = 0;
+	
+	while(a != "end") {
+		cin >> a;
+		if(a == "end") {
+			break;
+		}
+		
+		slova.insert(a);
+		
+		countMap[a]++;
+	}
+	
+	for (it = slova.begin(); it != slova.end(); ++it) {
+		if(countMap[*it] > 1) {
+			cout << *it << " ";
+		}
+	}
+	return 0;
+}
