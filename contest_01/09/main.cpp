de462829bec0ce9b1a22d5471fa0c6d3228135bb
Waffelzzz <109044711+Waffelzzz@users.noreply.github.com>
2023-10-01 14:31:10 +0300

    Update main.cpp

--- contest_01/09/main.cpp
+++ contest_01/09/main.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
 
+int main(){
+	
+	double a, b, c;
+	
+	cin >> a >> b;
+	
+	if(a > 0 && b > 0){
+		
+		c = sqrt(a*a + b*b);
+		printf("%0.14f", c);
+	}
+	return 0;
+}
