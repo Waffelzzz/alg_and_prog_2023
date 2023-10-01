25b6fc276ebb894b44c36ed2d4ce90aa19e4afc4
Waffelzzz <109044711+Waffelzzz@users.noreply.github.com>
2023-10-01 14:46:09 +0300

    Update main.cpp

--- contest_01/20/main.cpp
+++ contest_01/20/main.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
 
+
+int main() {
+    string num1, num2;
+
+    cin >> num1;
+
+    cin >> num2;
+
+    string sortedNum1 = num1;
+    string sortedNum2 = num2;
+    sort(sortedNum1.begin(), sortedNum1.end());
+    sort(sortedNum2.begin(), sortedNum2.end());
+
+    if (sortedNum1 == sortedNum2) {
+        cout << "YES";
+    } else {
+        cout <<  "NO";
+    }
+
+    return 0;
+}
