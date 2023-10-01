1ac82eb187a376998bff961bcf7fb7726325501c
Waffelzzz <109044711+Waffelzzz@users.noreply.github.com>
2023-10-01 14:11:05 +0300

    Update main.cpp

--- contest_01/01/main.cpp
+++ contest_01/01/main.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <math.h>
 
+using namespace std;
+
+
+int main() {
+	
+	float firstNumber = sqrt(12);
+	float secondNumber = (1 / (3.0*3.0));
+	float thirdNumber = (1 / (5*3.0*3.0));
+	float fourthNumber = (1 / (7*3.0*3.0*3.0));
+	float fifthNumber = (1 / (9*3.0*3.0*3.0*3.0));
+	float sixthNUmber = (1 / (11*3.0*3.0*3.0*3.0*3.0));
+	
+	double answer = (firstNumber * (1 - secondNumber + thirdNumber - fourthNumber + fifthNumber - sixthNUmber));
+	
+	printf("%f", answer);
+	
+	return 0;
+}
