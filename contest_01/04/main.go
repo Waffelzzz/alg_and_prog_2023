5337e6cddeee5b7498c5c368318c1b06d359cd7d
Waffelzzz <109044711+Waffelzzz@users.noreply.github.com>
2023-10-01 14:25:16 +0300

    Update main.go

--- contest_01/04/main.go
+++ contest_01/04/main.go
@@ -1,1 +1,18 @@
+package main
 
+import "fmt"
+
+func main() {
+
+	var a int
+	var b int
+	var proizv int
+
+	fmt.Scanf("%d", &a)
+	fmt.Scanf("%d", &b)
+
+	proizv = a * b
+
+	fmt.Printf("%d", proizv)
+
+}
