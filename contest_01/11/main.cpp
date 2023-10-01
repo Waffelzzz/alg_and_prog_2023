f3c432e3ab584d6b2b679bc1d2a9f8c60f919ac9
Waffelzzz <109044711+Waffelzzz@users.noreply.github.com>
2023-10-01 14:33:16 +0300

    Update main.cpp

--- contest_01/11/main.cpp
+++ contest_01/11/main.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
 
+int main(){
+	
+	int a, b, c;
+	
+	cin >> a >> b >> c;
+	
+	if(abs(a - b) < abs(a - c)) {
+		printf("B %d", abs(a - b));
+	} else {
+		printf("C %d", abs(a - c));	
+	}
+	return 0;
+}
