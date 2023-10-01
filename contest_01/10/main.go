baa3828bb673f1ca6bdc0d0cfdc720c7bd6517a3
Waffelzzz <109044711+Waffelzzz@users.noreply.github.com>
2023-10-01 14:31:53 +0300

    Update main.go

--- contest_01/10/main.go
+++ contest_01/10/main.go
@@ -1,1 +1,44 @@
+package main
 
+import "fmt"
+
+func main() {
+
+	var (
+		firstAnswer  string
+		secondAnswer string
+		thirdAnswer  string
+	)
+
+	fmt.Scanf("%s %s %s", &firstAnswer, &secondAnswer, &thirdAnswer)
+
+	if firstAnswer == "Да" {
+		if secondAnswer == "Да" {
+			if thirdAnswer == "Да" {
+				fmt.Printf("Утка")
+			} else {
+				fmt.Printf("Пингвин")
+			}
+		} else {
+			if thirdAnswer == "Да" {
+				fmt.Printf("Плезиозавры")
+			} else {
+				fmt.Printf("Дельфин")
+			}
+		}
+	} else {
+		if secondAnswer == "Да" {
+			if thirdAnswer == "Да" {
+				fmt.Printf("Страус")
+			} else {
+				fmt.Printf("Курица")
+			}
+		} else {
+			if thirdAnswer == "Да" {
+				fmt.Printf("Жираф")
+			} else {
+				fmt.Printf("Кот")
+			}
+		}
+	}
+}
