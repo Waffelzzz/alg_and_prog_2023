39fe6e1548457a7e89ee540c659191cf5e4ee2ef
Waffelzzz <109044711+Waffelzzz@users.noreply.github.com>
2023-10-01 14:14:10 +0300

    Update main.go

--- contest_01/02/main.go
+++ contest_01/02/main.go
@@ -1,1 +1,17 @@
+package main
 
+import "fmt"
+
+func main() {
+
+	var sredvgodchel float32
+	var sredvgodtopol int
+	var sredvgoddub int
+
+	sredvgodchel = 182.5
+	sredvgodtopol = 6
+	sredvgoddub = 10
+
+	fmt.Printf("%f %d %d", sredvgodchel, sredvgodtopol, sredvgoddub)
+
+}
