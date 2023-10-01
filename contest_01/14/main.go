5041b5cfe0fa7f4f951770f39efd8e6dc4c466e0
Waffelzzz <109044711+Waffelzzz@users.noreply.github.com>
2023-10-01 14:39:21 +0300

    Update main.go

--- contest_01/14/main.go
+++ contest_01/14/main.go
@@ -1,1 +1,46 @@
+package main
 
+import "fmt"
+
+func main() {
+	var row, col int
+
+	fmt.Scan(&col)
+
+	fmt.Scan(&row)
+
+	fmt.Printf("    |")
+
+	for i := 1; i <= row; i++ {
+		if i < 10 {
+			fmt.Printf("   %d", i)
+		} else {
+			fmt.Printf("  %d", i)
+		}
+	}
+
+	fmt.Println()
+	fmt.Printf("   --")
+	for i := 1; i <= 4*row; i++ {
+		fmt.Printf("-")
+	}
+	fmt.Println()
+
+	for j := 1; j <= col; j++ {
+		if j < 10 {
+			fmt.Printf("   %d|", j)
+		} else {
+			fmt.Printf("  %d|", j)
+		}
+		for k := 1; k <= row; k++ {
+			if k*j < 10 {
+				fmt.Printf("   %d", k*j)
+			} else if k*j < 100 {
+				fmt.Printf("  %d", k*j)
+			} else {
+				fmt.Printf(" %d", k*j)
+			}
+		}
+		fmt.Println()
+	}
+}
