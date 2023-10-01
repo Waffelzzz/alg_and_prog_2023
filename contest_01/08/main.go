815e0722e789dfd6c094b9be1ac7dc26bcb7b2d7
Waffelzzz <109044711+Waffelzzz@users.noreply.github.com>
2023-10-01 14:30:20 +0300

    Update main.go

--- contest_01/08/main.go
+++ contest_01/08/main.go
@@ -1,1 +1,27 @@
+package main
 
+import "fmt"
+
+func main() {
+
+	var (
+		a int
+		b int
+		c int
+	)
+
+	fmt.Scanf("%d %d %d", &a, &b, &c)
+
+	if a > b {
+		if a > c {
+			fmt.Printf("%d", a)
+		} else {
+			fmt.Printf("%d", c)
+		}
+	} else if b > c {
+		fmt.Printf("%d", b)
+	} else {
+		fmt.Printf("%d", c)
+	}
+
+}
