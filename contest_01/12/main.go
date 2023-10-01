7ecfe13bc54e3a553d65b5216012c2c003bec65c
Waffelzzz <109044711+Waffelzzz@users.noreply.github.com>
2023-10-01 14:37:03 +0300

    Update main.go

--- contest_01/12/main.go
+++ contest_01/12/main.go
@@ -1,1 +1,23 @@
+package main
 
+import "fmt"
+
+func main() {
+	var (
+		count int
+		X     int
+	)
+
+	fmt.Scanf("%d", &X)
+
+	for X != 1 {
+		if X%2 == 0 {
+			X /= 2
+		} else {
+			X = 3*X + 1
+		}
+		count++
+	}
+
+	fmt.Printf("%d", count)
+}
