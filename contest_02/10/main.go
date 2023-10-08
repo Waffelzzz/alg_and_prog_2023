73474d15c8917a41a2f9ed7926fa325e26c4449a
Waffelzzz <109044711+Waffelzzz@users.noreply.github.com>
2023-10-08 13:45:42 +0300

    Update main.go

--- contest_02/10/main.go
+++ contest_02/10/main.go
@@ -1,1 +1,23 @@
+func shift(data []int, steps int) {
+	length := len(data)
 
+	if steps > 0 {
+		steps %= length
+		data2 := make([]int, length)
+		for i := 0; i < length; i++ {
+			newIndex := (i + steps) % length
+			data2[newIndex] = data[i]
+		}
+		copy(data, data2)
+	}
+
+	if steps < 0 {
+		steps = (-steps) % length
+		data2 := make([]int, length)
+		for i := 0; i < length; i++ {
+			newIndex := (i - steps + length) % length
+			data2[newIndex] = data[i]
+		}
+		copy(data, data2)
+	}
+}
