d585af2c3e25d6ec0b30981894659f630baf6aa9
Waffelzzz <109044711+Waffelzzz@users.noreply.github.com>
2023-10-08 13:45:09 +0300

    Update main.go

--- contest_02/09/main.go
+++ contest_02/09/main.go
@@ -1,1 +1,23 @@
+func isLucky(num string) bool {
+	var (
+		count1 int
+		count2 int
+	)
 
+	count1 = 0
+	count2 = 0
+
+	for i := 0; i < 3; i++ {
+		count1 = count1 + int(num[i])
+	}
+
+	for j := 3; j < 6; j++ {
+		count2 = count2 + int(num[j])
+	}
+
+	if count1 == count2 {
+		return true
+	}
+
+	return false
+}
