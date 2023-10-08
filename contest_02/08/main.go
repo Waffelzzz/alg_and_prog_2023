5bf3cb0c730ba661d7ea401d69bbebdf2c70e524
Waffelzzz <109044711+Waffelzzz@users.noreply.github.com>
2023-10-08 13:44:35 +0300

    Update main.go

--- contest_02/08/main.go
+++ contest_02/08/main.go
@@ -1,1 +1,31 @@
+import "strings"
 
+func isPalindrome(s string) bool {
+	s = strings.ToLower(s)
+
+	i := 0
+	j := len(s) - 1
+
+	for i < j {
+		for i < j && !isAlphanumeric(s[i]) {
+			i++
+		}
+
+		for i < j && !isAlphanumeric(s[j]) {
+			j--
+		}
+
+		if s[i] != s[j] {
+			return false
+		}
+
+		i++
+		j--
+	}
+
+	return true
+}
+
+func isAlphanumeric(c byte) bool {
+	return (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')
+}
