aae318ccf86cd7e6a2333fe83766fe13accfb180
Waffelzzz <109044711+Waffelzzz@users.noreply.github.com>
2023-10-17 22:04:17 +0300

    Update main.cpp

--- contest_03/01/main.cpp
+++ contest_03/01/main.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
+int main() {
+
+	int x, y;
+	std::cin >> x;
+	std::vector<int> first(x);
+
+	for (int i = 0; i < x; i++) {
+		std::cin >> first[i];
+	}
+
+	std::cin >> y;
+	std::vector<int> second(y);
+
+	for (int j = 0; j < y; j++) {
+		std::cin >> second[j];
+	}
+
+	std::vector<int> result;
+	
+	for (int i = 0; i < x; i++) {
+		result.push_back(first[i]);
+	}
+
+	for (int i = 0; i < y; i++) {
+		result.push_back(second[i]);
+	}
+
+	sort(begin(result), end(result));
+
+	for (auto c : result) {
+		std::cout << c << ' ';
+	}
+
+	return 0;
+}
