69841888d41e76b535949e11c561e6cdbd9f8252
Waffelzzz <109044711+Waffelzzz@users.noreply.github.com>
2023-10-17 22:04:37 +0300

    Update main.cpp

--- contest_03/02/main.cpp
+++ contest_03/02/main.cpp
@@ -1,1 +1,45 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool isBalanced(const string& str) {
+    string opened;
+
+    for (char c : str) {
+        if (c == '(' || c == '[' || c == '{') {
+            opened += c;
+        }
+        else if (c == ')' || c == ']' || c == '}') {
+            if (opened.empty()) {
+                return false;
+            }
+
+            char lastopened = opened.back();
+            opened.pop_back();
+
+            if ((c == ')' && lastopened != '(') ||
+                (c == ']' && lastopened != '[') ||
+                (c == '}' && lastopened != '{')) {
+                return false;
+            }
+        }
+    }
+
+    return opened.empty();
+}
+
+int main() {
+    string str;
+    getline(cin, str, '!');
+
+    if (isBalanced(str)) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
+
+    return 0;
+}
 
