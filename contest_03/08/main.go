a3a2a863c961fb04a609c3d618c0195eaeeb573f
Waffelzzz <109044711+Waffelzzz@users.noreply.github.com>
2023-10-17 22:06:53 +0300

    Update main.go

--- contest_03/08/main.go
+++ contest_03/08/main.go
@@ -1,1 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num int
+	fmt.Scanln(&num)
+
+	counts := make(map[int]int)
+	for i := 0; i < num; i++ {
+		var num1 int
+		fmt.Scan(&num1)
+		counts[num1]++
+
+		if counts[num1] == 2 {
+			counts[num1] = 0
+		}
+	}
+
+	for j := range counts {
+		if counts[j] == 1 {
+			fmt.Println(j)
+			break
+		}
+	}
+}
 
