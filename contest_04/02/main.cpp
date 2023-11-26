51663bc0bed0223ce2c33ab1a6d0318a23c36556
bbishop <109044711+blxckbishop@users.noreply.github.com>
2023-11-26 14:36:36 +0300

    Update main.cpp

--- contest_04/02/main.cpp
+++ contest_04/02/main.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include "json.hpp"
+#include <fstream>
+
+using json = nlohmann::json;
+
+int main() {
+    std::ifstream input("data.json");
+
+    json data;
+    input >> data;
+
+    int newUserID;
+    std::cin >> newUserID;
+
+    int completedTasks = 0;
+    for (auto project : data) {
+        for (auto task : project["tasks"]) {
+            if (task["user_id"] == newUserID && task["completed"] == true) {
+                completedTasks++;
+            }
+        }
+    }
+
+    std::cout << completedTasks << std::endl;
+
+    return 0;
+}
 
